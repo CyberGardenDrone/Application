0ab46f9ee4e93d090eeded870729181031044659
Skrever <73542424+Skrever@users.noreply.github.com>
2024-06-09 05:32:05 +0300

    Good

--- Content/Scripts/menu.gd
+++ Content/Scripts/menu.gd
@@ -2,9 +2,13 @@ extends Control
 
 var ScreenViewer : Sprite2D
 
+var MoveConsoleTween : Tween
+var IsKonsoleUp = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	ScreenViewer = $Screen
+	MoveConsoleTween = create_tween()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,3 +17,16 @@ func _process(delta):
 
 func _lost_connection():
 	print("Connection lost")
+
+
+func _on_console_button_pressed():
+	if(!IsKonsoleUp):
+		MoveConsoleTween = get_tree().create_tween()
+		MoveConsoleTween.set_ease(Tween.EASE_IN_OUT)
+		MoveConsoleTween.tween_property($ConsolePanel,"position:y",0, 0.5)
+		IsKonsoleUp = true
+	else:
+		MoveConsoleTween = get_tree().create_tween()
+		MoveConsoleTween.set_ease(Tween.EASE_IN_OUT)
+		MoveConsoleTween.tween_property($ConsolePanel,"position:y",925, 0.5)
+		IsKonsoleUp = false
